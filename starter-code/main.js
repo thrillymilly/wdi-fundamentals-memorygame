e03e3887454fe350aad956f159fad7ddd24303b7
William Miller <williammiller@williams-mbp.home>
2017-01-18 10:18:01 +1100

    created cards and board

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,24 +1,19 @@
-console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
 
-var cardTwo = "queen";
+var createCards = function(){
 
-var cardThree = "king";
+var board = document.querySelector('game-board');
 
-var cardFour = "king";
+function createBoard() {
+  for (i=0; i<cards.length; i++) {
 
-/* ==== Use for the next assignment
-if (cardOne === cardTwo) {
-	alert('You found a match'); 
-} else {
-	alert('Sorry, try again')
-};
+    var cardElement = document.createElement('div');
 
-if (cardThree === cardFour) {
-	alert('You found a match'); 
-} else {
-	alert('Sorry, try again');
-};
-*/
+    cardElement.className = 'card';
 
+    board.appendChild(cardElement);
+  }
+
+}
+
+}
